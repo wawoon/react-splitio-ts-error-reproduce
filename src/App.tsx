5a805b52a7144edde38b5096734d4b5e038f3c6f
Yoshinori Kosaka <yoshinori.kosaka@welyco.com>
2019-10-02 17:49:36 +0900

    add error cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Split } from "react-splitio";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* fixed in v0.1.9 */}
+      <Split name="my-split">
+        {value => {
+          return <div>hello, world</div>;
+        }}
+      </Split>
+      {/* These two cases doen't work in v0.1.9 */}
+      <Split name="my-split">
+        {value => {
+          return null;
+        }}
+      </Split>
+      <Split name="my-split">
+        {value => {
+          if (value && value.treatment === "on") {
+            return <div></div>;
+          } else {
+            return null;
+          }
+        }}
+      </Split>
     </div>
   );
-}
+};
 
 export default App;
